4ba3771971a260e8508bfbd3df5bd53a7ba83067
ewinger581 <44706518+ewinger581@users.noreply.github.com>
2019-10-31 07:49:32 -0500

    Create RequiredServices.sh

---
+++ RequiredServices.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#### uwu ####
+echo -n "is ssh a required service? [Y/n] "
+read -r option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  apt-get install ssh
+  apt-get install openssh-server
+else
+  apt-get remove ssh
+  apt-get remove openssh-server
+  apt-get autoremove
+  exit
+fi
+  exit
+  
+#### XD ####
+echo -n "Is apache required? [Y/n] "
+read -r option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  apt-get install apache2
+else
+  apt-get remove apache2
+  exit
+fi
+  exit
+  
+#### oWo ####
+echo -n "Sql? [Y/n] "
+read -r option
+if [[ $option =~ ^[Yy]$ ]]
+then
+  exit
+else
+  apt-get remove --purge mysql
+  exit
+fi
+  exit
+
+
