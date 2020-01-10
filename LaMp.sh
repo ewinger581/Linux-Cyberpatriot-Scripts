6abe5769a8ec2582932f3a55e1601752da766211
ewinger581 <44706518+ewinger581@users.noreply.github.com>
2020-01-10 07:57:07 -0600

    Update LaMp.sh

--- LaMp.sh
+++ LaMp.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+#Linux
+#Apache
+#Mysql
+#PHP
+#Securo
+sudo ufw allow in 'Apache Full'
+#Add WAF
+sudo apt-get install libapache2-mod-security2 libapache2-mod-evasive
+#SQL
+sudo apt-get install mysql-server
+sudo mysql_install_db
+sudo mysql_secure_installation
+#Secure SQL in the /etc/mysql/my.cnf make sure bing address is loopback and use strat to secure
+#Connect to localhost with sql and secure
+#CREATE USER ‘noob’@’localhost’ IDENTIFIED BY ‘noobpassword’;
+#SHOW grants FOR ‘noob’@’localhost;
+#https://opensourceforu.com/2019/05/securing-a-lamp-server-in-2019/
