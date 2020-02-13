7e01f9ee2245a703b164c4155c24f86a434a6272
ewinger581 <44706518+ewinger581@users.noreply.github.com>
2020-02-12 18:56:58 -0600

    Update pass.sh

--- pass.sh
+++ pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Are you a bum who doesn't want to do passwords manually? well here u go bum boy
 awk -F: '($3>=1000){print $1}' /etc/passwd | sort > /root/userlist.txt
 echo root >> /root/userlist.txt
 sed '/nobody/d' /root/userlist.txt
@@ -7,6 +7,6 @@ sed '/nobody/d' /root/userlist.txt
 for i in $(cat /root/userlist.txt)
 do
 echo $i 
-echo "$i":TigerTiger1! | /usr/sbin/chpasswd
+echo "$i":Change to yo password stoopid | /usr/sbin/chpasswd
 echo; echo "User $i's password changed!"
 done
